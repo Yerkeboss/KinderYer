e2549e4fa0de3d372bbc783c7fd542372c77b1bd
Yerke <zh.yerkegaly@gmail.com>
2024-11-27 00:31:03 +0500

    chan

--- src/components/FaqAccordion/FaqAccordion.jsx
+++ src/components/FaqAccordion/FaqAccordion.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import './FaqAccordion.css';
 import Accordion from 'react-bootstrap/Accordion';
+import { useTranslation } from 'react-i18next';
 
 function FaqAccordion() {
+    const { t } = useTranslation();
   return (
     <div className='faq-section'>
         <div className='container d-flex flex-column align-items-center'>
-            <h2 className='text-center text-capitalize mb-5'>Frequently asked questions</h2>
-            <p className='text-center mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat numquam rem expedita similique necessitatibus dolore magnam natus sit ipsa, quae neque accusantium placeat laudantium mollitia, reprehenderit alias incidunt, delectus obcaecati cupiditate sed omnis repudiandae eos! Quasi unde veritatis labore quibusdam.</p>
+            <h2 className='text-center text-capitalize mb-5'>{t('question-header')}</h2>
+            <p className='text-center mb-5'>{t('question-header-info')}</p>
             <Accordion defaultActiveKey="" flush>
                 <Accordion.Item eventKey='0'>
-                    <Accordion.Header>First question ?</Accordion.Header>
+                    <Accordion.Header><p>{t('first-question')}</p></Accordion.Header>
                     <Accordion.Body>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo ducimus quod qui eius nobis quam temporibus similique fugit? Molestias, illo, maxime nulla aliquid ut pariatur corrupti aspernatur dolor assumenda quos officia eos?
+                    <p>{t('first-answer')}</p>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='1'>
-                    <Accordion.Header>Second question ?</Accordion.Header>
+                <Accordion.Header><p>{t('second-question')}</p></Accordion.Header>
                     <Accordion.Body>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo ducimus quod qui eius nobis quam temporibus similique fugit? Molestias, illo, maxime nulla aliquid ut pariatur corrupti aspernatur dolor assumenda quos officia eos?
+                    <p>{t('second-answer')}</p>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='2'>
-                    <Accordion.Header>Third question ?</Accordion.Header>
+                <Accordion.Header><p>{t('third-question')}</p></Accordion.Header>
                     <Accordion.Body>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo ducimus quod qui eius nobis quam temporibus similique fugit? Molestias, illo, maxime nulla aliquid ut pariatur corrupti aspernatur dolor assumenda quos officia eos?
+                    <p>{t('third-answer')}</p>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey='3'>
-                    <Accordion.Header>Fourth question ?</Accordion.Header>
+                <Accordion.Header><p>{t('fourth-question')}</p></Accordion.Header>
                     <Accordion.Body>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo ducimus quod qui eius nobis quam temporibus similique fugit? Molestias, illo, maxime nulla aliquid ut pariatur corrupti aspernatur dolor assumenda quos officia eos?
+                    <p>{t('fourth-answer')}</p>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='4'>
+                <Accordion.Header><p>{t('fifth-question')}</p></Accordion.Header>
+                    <Accordion.Body>
+                    <p>{t('fifth-answer')}</p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -33,75 +33,79 @@ const blogs = [
 
 function Home() {
     const { t } = useTranslation(); // Use the translation hook
-  return (
-    <div className='home-page'>
-        <header className='h-100 d-flex align-items-center text-light'>
-            <div className='container d-flex flex-column align-items-center' style = {{marginTop:'4rem'}}>
-                <h2>{t('welcome1')}</h2>
-                <br/>
-                <p>{t('welcome2')}</p>
-                <br/>
-                <div className='d-flex flex-column flex-sm-row align-items-center'>
-                    <Link to="/courses">
-                        <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0'> {t('subjects')}</button>
-                    </Link>
-                    <Link to="/contact">
-                        <button type='button' className='btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>{t('contactus')}</button>
-                    </Link>
-                </div>
-            </div>
-        </header>
-
-        <div className="py-5">
-            <ChooseSection />
-        </div>
-
-        <div className='py-5 bg-light'>
-            <div className="container">
-                <div className='row d-flex align-items-center justify-content' style = {{justifyContent:'space-between', marginLeft:'2rem'}}>
-                    <div className='col-lg-5'>
-                        <h2 style = {{fontWeight:'bolder'}}className='text-capitalize'>{t('course-home')}</h2>
-                        <p style = {{marginTop:'1rem'}}>{t('course-home-info')}</p>
+    return (
+        <div className='home-page'>
+            <header className='h-100 d-flex align-items-center text-light'>
+                <div className='container d-flex flex-column align-items-center' style={{ marginTop: '4rem' }}>
+                    <h2>{t('welcome1')}</h2>
+                    <br />
+                    <p>{t('welcome2')}</p>
+                    <br />
+                    <div className='d-flex flex-column flex-sm-row align-items-center'>
                         <Link to="/courses">
-                            <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-3'>{t('course-info-btn')}</button>
+                            <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0'> {t('subjects')}</button>
+                        </Link>
+                        <Link to="/contact">
+                            <button type='button' className='btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>{t('contactus')}</button>
                         </Link>
                     </div>
-                    <div className='col-lg-5 mt-5 mt-lg-0'>
-                        <img src={CourseInfo} style= {{borderRadius:'50%', height:'30rem', width:'30rem'}} alt="" />
+                </div>
+            </header>
+
+            <div className="py-5">
+                <ChooseSection />
+            </div>
+
+            <div className='py-5 bg-light'>
+                <div className="container">
+                    <div className='row d-flex align-items-center justify-content' style={{ justifyContent: 'space-between', margin:'2rem' }}>
+                        <div className='col-lg-5'>
+                            <h2 style={{ fontWeight: 'bolder' }} className='text-capitalize'>{t('course-home')}</h2>
+                            <p style={{ marginTop: '1rem' }}>{t('course-home-info')}</p>
+                            <Link to="/courses">
+                                <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-3'>{t('course-info-btn')}</button>
+                            </Link>
+                        </div>
+                        <div className='col-lg-5 mt-5 mt-lg-0' style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                            <img
+                                src={CourseInfo}
+                                alt=""
+                                className='home-course-header'
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="py-5">
-            <FaqAccordion />
-        </div>
+            <div className="py-5">
+                <FaqAccordion />
+            </div>
 
-        <div className='blog-section text-light py-5'>
-            <div className='container d-flex flex-column align-items-center'>
-                <h2 className='text-center text-capitalize mb-5'>Latest on the blog</h2>
-                <div className='row g-4'>
-                    {blogs.map((blog) => (
-                        <div key={blog.id} className='col-md-6 col-lg-4'>
-                            <Link to="/blog" className='text-decoration-none'>
-                                <Card className='h-100 shadow scale-hover-effect'>
-                                    <Card.Img variant="top" src={blog.img} />
-                                    <Card.Body className='p-md-5'>
-                                        <Card.Title>{blog.title}</Card.Title>
-                                        <Card.Text>{blog.description}</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </div>
-                    ))}
+            <div className='blog-section text-light py-5'>
+                <div className='container d-flex flex-column align-items-center'>
+                    <h2 className='text-center text-capitalize mb-5'>Latest on the blog</h2>
+                    <div className='row g-4'>
+                        {blogs.map((blog) => (
+                            <div key={blog.id} className='col-md-6 col-lg-4'>
+                                <Link to="/blog" className='text-decoration-none'>
+                                    <Card className='h-100 shadow scale-hover-effect'>
+                                        <Card.Img variant="top" src={blog.img} />
+                                        <Card.Body className='p-md-5'>
+                                            <Card.Title>{blog.title}</Card.Title>
+                                            <Card.Text>{blog.description}</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                    <Link to="/blog">
+                        <button type='button' className='btn btn-danger btn-lg mt-5'>Read More Blogs</button>
+                    </Link>
                 </div>
-                <Link to="/blog">
-                    <button type='button' className='btn btn-danger btn-lg mt-5'>Read More Blogs</button>
-                </Link>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home;
