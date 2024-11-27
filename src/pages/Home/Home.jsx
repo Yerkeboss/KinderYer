55fc1a02c54ef588d03fd3ac362de7ffc5f03fed
Yerke <zh.yerkegaly@gmail.com>
2024-11-27 08:43:08 +0500

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,35 +4,35 @@ import ChooseSection from '../../components/ChooseSection/ChooseSection';
 import CourseInfo from '../../utils/images/courses1.jpg';
 import FaqAccordion from '../../components/FaqAccordion/FaqAccordion';
 import { Card } from 'react-bootstrap';
-import Blog1Img from '../../utils/images/blog1-img.jpg';
-import Blog2Img from '../../utils/images/blog2-img.jpg';
-import Blog3Img from '../../utils/images/blog3-img.jpg';
+import Blog1Img from '../../utils/images/album1.jpg';
+import Blog2Img from '../../utils/images/alb13.jpg';
+import Blog3Img from '../../utils/images/alb9.jpg';
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 
-const blogs = [
-    {
-        id: 1,
-        img: [Blog1Img],
-        title: 'Blog 1',
-        description: 'Lorem ipsum doknlor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
-    },
-    {
-        id: 2,
-        img: [Blog2Img],
-        title: 'Blog 2',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
-    },
-    {
-        id: 3,
-        img: [Blog3Img],
-        title: 'Blog 3',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
-    }
-];
-
 function Home() {
     const { t } = useTranslation(); // Use the translation hook
+    const blogs = [
+        {
+            id: 1,
+            img: [Blog1Img],
+            title: `${t('blog1-title')}`,
+            description: `${t('blog1-descr')}`
+        },
+        {
+            id: 2,
+            img: [Blog2Img],
+            title: `${t('blog2-title')}`,
+            description: `${t('blog2-descr')}`
+        },
+        {
+            id: 3,
+            img: [Blog3Img],
+            title: `${t('blog3-title')}`,
+            description: `${t('blog3-descr')}`
+        }
+    ];
+
     return (
         <div className='home-page'>
             <header className='h-100 d-flex align-items-center text-light'>
@@ -58,15 +58,15 @@ function Home() {
 
             <div className='py-5 bg-light'>
                 <div className="container">
-                    <div className='row d-flex align-items-center justify-content' style={{ justifyContent: 'space-between', margin:'2rem' }}>
+                    <div className='row d-flex align-items-center justify-content' style={{ justifyContent: 'space-between', margin: '2rem' }}>
                         <div className='col-lg-5'>
                             <h2 style={{ fontWeight: 'bolder' }} className='text-capitalize'>{t('course-home')}</h2>
-                            <p style={{ marginTop: '1rem' }}>{t('course-home-info')}</p>
+                            <p style={{ marginTop: '1rem', textAlign: 'justify' }}>{t('course-home-info')}</p>
                             <Link to="/courses">
                                 <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-3'>{t('course-info-btn')}</button>
                             </Link>
                         </div>
-                        <div className='col-lg-5 mt-5 mt-lg-0' style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                        <div className='col-lg-5 mt-5 mt-lg-0' style={{ justifyContent: 'flex-end', alignItems: 'center', display: 'flex' }}>
                             <img
                                 src={CourseInfo}
                                 alt=""
@@ -83,16 +83,16 @@ function Home() {
 
             <div className='blog-section text-light py-5'>
                 <div className='container d-flex flex-column align-items-center'>
-                    <h2 className='text-center text-capitalize mb-5'>Latest on the blog</h2>
+                    <h2 className='text-center text-capitalize mb-5'>{t('blog')}</h2>
                     <div className='row g-4'>
                         {blogs.map((blog) => (
                             <div key={blog.id} className='col-md-6 col-lg-4'>
                                 <Link to="/blog" className='text-decoration-none'>
                                     <Card className='h-100 shadow scale-hover-effect'>
-                                        <Card.Img variant="top" src={blog.img} />
+                                        <Card.Img className="blog-header-img" variant="top" src={blog.img} />
                                         <Card.Body className='p-md-5'>
-                                            <Card.Title>{blog.title}</Card.Title>
-                                            <Card.Text>{blog.description}</Card.Text>
+                                            <Card.Title style={{ fontWeight: 'bolder' }}>{blog.title}</Card.Title>
+                                            <Card.Text style={{ textAlign: 'justify' }}>{blog.description}</Card.Text>
                                         </Card.Body>
                                     </Card>
                                 </Link>
@@ -100,7 +100,7 @@ function Home() {
                         ))}
                     </div>
                     <Link to="/blog">
-                        <button type='button' className='btn btn-danger btn-lg mt-5'>Read More Blogs</button>
+                        <button type='button' className='btn btn-danger btn-lg mt-5'>{t('blog-btn')}</button>
                     </Link>
                 </div>
             </div>
