dfd46ab7058ca0c9da20292987791a1b54e9615e
Yerkeboss <zh.yerkegaly@gmail.com>
2026-08-13 15:05:57 +0500

    changes123

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import './Contact.css';
 import { useTranslation } from 'react-i18next';
-import useSEO from '../../hooks/useSEO';
 
 function Contact() {
     const { t } = useTranslation();
-    useSEO(t('contact'), t('contact-meta-description'));
     return (
         <div className='contact-page'>
             <header className='height-75'>
                 <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
                     <h1 className='container2 text-center fw-semibold'style ={{marginTop:'6rem'}}>{t('contactus')}</h1>
                     <p className='text-center w-75 mb-5' style={{ marginTop: '2rem', fontWeight: 'bold', fontSize: '1.2rem' }}>{t('address')}</p>
-                    <p className='text-center w-75 mb-5' style={{ marginTop: '0rem', fontWeight: 'bold', fontSize: '1.2rem' }}>{t('phone')}</p>
-                    <p className='text-center w-75 mb-5' style={{ marginTop: '0rem', fontWeight: 'bold', fontSize: '1.2rem' }}>Email: zh.yerkegaly@gmail.com</p>
+                    <p className='text-center w-75 mb-5' style={{ marginTop: '0rem', fontWeight: 'bold', fontSize: '1.2rem' }}>+7 705 565 6404</p>
+                    <p className='text-center w-75 mb-5' style={{ marginTop: '0rem', fontWeight: 'bold', fontSize: '1.2rem' }}>Email: ersan.zhanai22@yandex.kz</p>
                 </div>
             </header>
             <div className='bg-dark text-light p-5'>
                 <div className='container'>
                     <div className='mt-5'>
-                        <div className='d-flex justify-content-center mt-4 w-100' style={{ maxWidth: '800px', aspectRatio: '4 / 3', margin: '0 auto' }}>
+                        <div className='d-flex justify-content-center mt-4'>
 
-                            <iframe title="Unique Title" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5828.339921110473!2d71.44595854662604!3d43.07992203454041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38a7116ad35e0b3d%3A0x3030e6c529776568!2z0JzRi9GA0LfQsNGC0LDQuQ!5e0!3m2!1sru!2skz!4v1732811751683!5m2!1sru!2skz" width="100%" height="100%" style={{ border: 0 }}></iframe>
+                            <iframe title="Unique Title" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5828.339921110473!2d71.44595854662604!3d43.07992203454041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38a7116ad35e0b3d%3A0x3030e6c529776568!2z0JzRi9GA0LfQsNGC0LDQuQ!5e0!3m2!1sru!2skz!4v1732811751683!5m2!1sru!2skz" width="800" height="600"></iframe>
                         </div>
                     </div>
 
